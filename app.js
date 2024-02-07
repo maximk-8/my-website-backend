1aefd9e9f236cefd351851d4983d21759a7145d8
maximkondrashuk <mkondras@ucsd.edu>
2024-02-07 00:48:18 -0800

    backend for graphAPI

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const express = require('express');
+const cors = require('cors');
+const { exec } = require('child_process');
+const fs = require('fs');
 const app = express();
 
-app.listen(4000, () => {
-    console.log('Server is running on port 4000');
-});
+app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+app.listen(4000);
+
+app.post('/api/findPath', (req, res) => {
+    fs.writeFile('temp.csv', req.body.graph, (err) => {
+        if (err) throw err;
+
+        req.body.nodes.sort((a, b) => Number(a) - Number(b));
+
+        exec(`./Graph temp.csv ${req.body.term} ${req.body.nodes[0]} ${req.body.nodes[1]}`, (error, stdout, stderr) => {
+            if (error) {
+                console.error(`exec error: ${error}`);
+                return;
+            }
+            res.json({result: stdout.trim()});
+        });
+    });
 });
